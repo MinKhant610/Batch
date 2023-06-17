8c7664c2ad10d20668e9e9bca50c485355cbc7cb
MinKhant610 <minkhantiopp@gmail.com>
2023-06-17 10:47:28 +0630

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+
+using namespace std;
+
+int main(){
+    /*
+    bool => 1bytes
+    int => 2 or 4 bytes 
+    float => 4bytes 
+    double => 8bytes 
+    string => 4bytes 
+    1bytes => 8bits
+    */
+   cout << "hello wordl" << endl;
+    return 0;
+}
